f1cc942d1189cdb0964e8fd1b089f69123ccb859
GamerMikeFun <164586859+GamerMikeFun@users.noreply.github.com>
2024-04-03 18:54:14 -0400

    changed welcome message

--- js/commands.js
+++ js/commands.js
@@ -59,7 +59,7 @@ banner = [
     "Terminal.js",
   "better banner coming soon",
                                                                                                                
-  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">Welcome to the GamerMike web terminal.</span>',
   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ];
 
